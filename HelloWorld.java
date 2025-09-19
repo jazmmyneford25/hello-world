3db68d5e5d1ccf3b9f8c77d298db0b7c9927ed47
jazmynne ford <jford044@lab.cpcc.edu>
2025-09-19 12:10:17 -0400

    Added second print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
     }
 }
+
 
 
